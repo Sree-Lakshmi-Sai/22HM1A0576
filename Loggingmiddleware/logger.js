d0e1d80daa619977277ec54ff564fe0cb53a74e9
Sree-Lakshmi-Sai <sreelakshmisai15@gmail.com>
2025-09-06 15:57:18 +0530

    Add files via upload

--- Loggingmiddleware/logger.js
+++ Loggingmiddleware/logger.js
@@ -1,1 +1,7 @@
-
+// src/utils/logger.js
+export function logEvent(event, details = {}) {
+  const message = [${new Date().toISOString()}] EVENT: ${event} ${JSON.stringify(details)};
+  // Optionally send this to a backend or external service in the future
+  // For now, log to the browser console as a proof of integration
+  console.info(message);
+}
